69514eab91da26bb9f9f7b4b6095ee7f0f60a82a
junngithub <jsenngooi@gmail.com>
2025-01-19 22:16:38 +0800

    level 1 commit

--- src/main/java/Icarus.java
+++ src/main/java/Icarus.java
@@ -1,17 +1,39 @@
-public class Duke {
-    public String greet() {
-        return """ 
-                ---------------------- \n
+import java.util.Scanner;
+
+public class Icarus {
+    public static void greet() {
+        String greeting = """ 
+                ----------------------
                 Hello! I am Icarus. \n
                 What can I do for you?
-                ---------------------- \n
+                ----------------------
                 """;
+        System.out.println(greeting);
     }
 
-    public String sayBye() {
-        return "Bye! See you next time!";
+    public static void sayBye() {
+        String bye = """
+                -----------------------
+                Bye! See you next time!
+                -----------------------
+                """;
+        System.out.println(bye);
     }
+
+    public static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        if (userInput.equals("bye")) {
+            sayBye();
+        } else {
+            System.out.println("\n" + userInput + "\n");
+            echo();
+        }
+
+    }
+
     public static void main(String[] args) {
-        System.out.println(greet());
+        greet();
+        echo();
     }
 }
